7d7086225ab4bf35d93942f17e44e9670280c4c1
srikanth5738 <s.kotamarthi@iveond.com>
2022-08-09 13:04:51 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import ListUserComponent from './components/ListUserComponent';
+//import HeaderComponent from './components/HeaderComponent';
+import FooterComponent from './components/FooterComponent';
+import CreateUserComponent from './components/CreateUserComponent';
+import ViewUserComponent from './components/ViewUserComponent';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <Router>
+              <HeaderComponent />
+                <div className="container">
+                    <Switch> 
+  <Route path = "/" exact component = {ListUserComponent}></Route>
+  <Route path = "/users" component = {ListUserComponent}></Route>
+  <Route path = "/add-user/:id" component = {CreateUserComponent}>
+        </Route>
+  <Route path = "/view-user/:id" component = {ViewUserComponent}>
+        </Route>
+                    </Switch>
+                </div>
+              <FooterComponent />
+        </Router>
     </div>
+    
   );
 }
 
